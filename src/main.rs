f284793cc6201708ac41b71479a1c4b3e456831f
bp42s <bransonyflynn@gmail.com>
2024-10-27 15:10:01 -0400

    added localhost functionality, made it work

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,37 @@ mod image;
 #[path = "forms/listing.rs"]
 mod listing;
 
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+#[allow(unused_imports)]
+use std::{fs, net::SocketAddr};
 
-async fn handle_page_home() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+// send html/ejs content for: index
+async fn handle_page_index() -> impl IntoResponse {
+    match fs::read_to_string("views/index.ejs") {
+        Ok(content) => Html(content),
+        Err(e) => Html(format!("Error : {}", e)),
+    }
 }
 
-async fn handle_page_about() -> Html<&'static str> {
-    Html("<p>about page</p>")
+// send html/ejs content for: about
+async fn handle_page_about() -> impl IntoResponse {
+    match fs::read_to_string("views/about.ejs") {
+        Ok(content) => Html(content),
+        Err(e) => Html(format!("Error : {}", e)),
+    }
 }
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let _app: Router = Router::new()
-        .route("/", get(handle_page_home))
+    let router: Router = Router::new()
+        .route("/", get(handle_page_index))
         .route("/about", get(handle_page_about));
-
-    // run it with hyper on localhost:3000
-    let router = Router::new().route("/", get(handle_page_home));
+    // run on localhost:3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, router).await.unwrap();
 }
