b00f622317ff6291480ec51727eaf7e7d1b6188d
bp42s <bransonyflynn@gmail.com>
2024-10-27 16:34:28 -0400

    fixed 404 and setup routing handling

--- src/main.rs
+++ src/main.rs
@@ -15,17 +15,23 @@ use axum::{
 #[allow(unused_imports)]
 use std::{fs, net::SocketAddr};
 
-// send html/ejs content for: index
-async fn handle_page_index() -> impl IntoResponse {
+// handler functions - sends html/ejs content for requests
+async fn handler_index() -> impl IntoResponse {
     match fs::read_to_string("views/index.ejs") {
         Ok(content) => Html(content),
         Err(e) => Html(format!("Error : {}", e)),
     }
 }
 
-// send html/ejs content for: about
-async fn handle_page_about() -> impl IntoResponse {
+async fn handler_about() -> impl IntoResponse {
     match fs::read_to_string("views/about.ejs") {
+        Ok(content) => Html(content),
+        Err(e) => Html(format!("Error : {}", e)),
+    }
+}
+
+async fn handler_404() -> impl IntoResponse {
+    match fs::read_to_string("views/404.ejs") {
         Ok(content) => Html(content),
         Err(e) => Html(format!("Error : {}", e)),
     }
@@ -33,11 +39,12 @@ async fn handle_page_about() -> impl IntoResponse {
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
     let router: Router = Router::new()
-        .route("/", get(handle_page_index))
-        .route("/about", get(handle_page_about));
-    // run on localhost:3000
+        .route("/", get(handler_index))
+        .route("/about", get(handler_about))
+        .fallback(handler_404);
+
+    // runs on localhost:3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, router).await.unwrap();
 }
