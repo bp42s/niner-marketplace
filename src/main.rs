ee2e1acb54ddd8027e55b6c2456a386bf17e6cb7
bp42s <140995694+bp42s@users.noreply.github.com>
2024-10-27 20:21:11 -0400

    formatted ejs

--- src/main.rs
+++ src/main.rs
@@ -37,12 +37,15 @@ async fn handler_404() -> impl IntoResponse {
 
 #[tokio::main]
 async fn main() {
+    // initialize the server and handling
     let router: Router = Router::new()
         .route("/", get(handler_index))
         .route("/about", get(handler_about))
         .fallback(handler_404);
 
-    // runs on localhost:3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    // startup - runs on https://localhost:3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .unwrap();
     axum::serve(listener, router).await.unwrap();
 }
